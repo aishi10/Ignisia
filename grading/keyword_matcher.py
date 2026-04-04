9366f52672b114b0f2dace3b83c7f72603c55c5f
Aishi Chakraborty <aishi.mitblr2024@learner.manipal.edu>
2026-04-04 09:53:26 +0530

    Delete grading directory

--- grading/keyword_matcher.py
+++
@@ -1,9 +0,0 @@
-from rapidfuzz import fuzz
-
-def keyword_score(student_text, keywords):
-    score = 0
-    for word in keywords:
-        match = fuzz.partial_ratio(word.lower(), student_text.lower())
-        if match > 80:
-            score += 1
-    return score / len(keywords) if keywords else 0
